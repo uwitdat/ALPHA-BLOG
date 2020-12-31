74a0b8935a61b70399faf7993328ed30f5411b5e
Ben Shekhtman <benshekhtman@hotmail.com>
2020-12-31 01:45:11 -0500

    edit articles table

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,14 +1,45 @@
 class ArticlesController < ApplicationController
 
-def show 
-  @article = Article.find(params[:id])
+    def show 
+        @article = Article.find(params[:id])
+    end
+
+    def index 
+        @articles = Article.all 
+    end
+
+        def new 
+            @article = Article.new
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
+    def create 
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save 
+        flash[:notice] = "Article was created succesfully"
+        redirect_to @article
+        else 
+            render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])   
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
 end
 
-def index 
-  @articles = Article.all 
-end
 
-end
+
 
 
 
