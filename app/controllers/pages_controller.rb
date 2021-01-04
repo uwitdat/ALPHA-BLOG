635a3a73800b847a847951958168012ea3fab2b9
Ben Shekhtman <benshekhtman@hotmail.com>
2021-01-03 20:09:15 -0500

    App production ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
-    def home
-        render html: '<h1>Alpha Blog App!</h1>'.html_safe
-    end
-    def about
-    end
+    def home; end
+     
+    def about; end
+ 
 end
